45023bcdc0e3d4d0fcb90ebad640a9a057ede7d0
AyupovTimur <timurayupov09@gmail.com>
2025-02-11 14:37:06 +0400

    class components are rewritten to functional components

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -3,40 +3,36 @@ import RandomChar from "../randomChar/RandomChar";
 import CharList from "../charList/CharList";
 import CharInfo from "../charInfo/CharInfo";
 import decoration from "../../resources/img/vision.png";
-import { Component } from "react";
+import { useState } from "react";
 import ErrorBoundary from "../errorBoundary/ErrorBoundary";
+import PropTypes from "prop-types";
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      charId: null,
-    };
-  }
+const App = () => {
+  const [charId, setCharId] = useState(null);
 
-  getCharId = (id) => {
-    this.setState({
-      charId: id,
-    });
+  const getCharId = (id) => {
+    setCharId(id);
   };
 
-  render() {
-    return (
-      <div className="app">
-        <AppHeader />
-        <main>
-          <RandomChar />
-          <div className="char__content">
-            <CharList getCharId={this.getCharId} />
-            <ErrorBoundary>
-              <CharInfo id={this.state.charId} />
-            </ErrorBoundary>
-          </div>
-          <img className="bg-decoration" src={decoration} alt="vision" />
-        </main>
-      </div>
-    );
-  }
-}
+  return (
+    <div className="app">
+      <AppHeader />
+      <main>
+        <RandomChar />
+        <div className="char__content">
+          <CharList getCharId={getCharId} />
+          <ErrorBoundary>
+            <CharInfo id={charId} />
+          </ErrorBoundary>
+        </div>
+        <img className="bg-decoration" src={decoration} alt="vision" />
+      </main>
+    </div>
+  );
+};
+
+App.propTypes = {
+  charId: PropTypes.func,
+};
 
 export default App;
